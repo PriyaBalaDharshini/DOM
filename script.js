af7a640b05b917d52095a2b3c592e1ed3cc13250
PriyaBalaDharshini <priyaarasu12@gmail.com>
2023-09-16 14:45:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function foo() {
+    let fname = document.getElementById("firstname").value;
+    let mname = document.getElementById("middlename").value
+    let lname = document.getElementById("lastname").value
+    let email = document.getElementById("email").value
+    let pin = document.getElementById("pin").value
+
+    console.log(fname, mname, lname, email, pin);
+}
